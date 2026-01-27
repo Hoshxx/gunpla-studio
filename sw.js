17687d4c996b6944cb457be62d093790caf5e7ec
Hoshxx <139757320+Hoshxx@users.noreply.github.com>
2026-01-27 17:03:46 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'gunpla-studio-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://img.icons8.com/color/512/paint-bucket.png'
+];
+
+// Install the Service Worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Intercept requests to serve from cache
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
